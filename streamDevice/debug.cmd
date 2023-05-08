3dbbf3e2d1c5cc9cce25221b474bf311b5278725
Scott Feister <sfeister@gmail.com>
2023-05-08 18:53:11 +0000

    Added a debug device with only IDN

---
+++ streamDevice/debug.cmd
@@ -0,0 +1,28 @@
+exec O.$EPICS_HOST_ARCH/streamApp $0
+dbLoadDatabase "O.Common/streamApp.dbd"
+streamApp_registerRecordDeviceDriver
+
+
+epicsEnvSet "STREAM_PROTOCOL_PATH","."
+
+drvAsynSerialPortConfigure("debug_ino","/dev/ttyUSB-arduino1.1")
+asynSetOption("debug_ino",0,"baud","115200")
+asynSetOption("debug_ino",0,"bits","8")
+asynSetOption("debug_ino",0,"parity","none")
+asynSetOption("debug_ino",0,"stop","1")
+asynSetOption("debug_ino",0,"clocal","Y")
+asynSetOption("debug_ino",0,"crtscts","N")
+
+#drvAsynIPPortConfigure "LO","localhost:40000"
+#vxi11Configure "LO","192.168.1.236",0,0.0,"gpin0"
+
+epicsThreadSleep(5) # empirically necessary to pause here, to avoid communication timeout on first SCPI commands to Arduinos. -SKF April 5 2022
+
+dbLoadRecords "debug.db"
+
+#log debug output to file
+#streamSetLogfile StreamDebug.log
+
+iocInit
+
+#var streamDebug 1
